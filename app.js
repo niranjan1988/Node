64c038380171bd8665c2fc3fe6453d3356ba7513
Niranjan <valliveduniranjan@gmail.com>
2020-10-10 22:11:04 +0530

    Using body-parser and app.get/app.post

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const port = 3000;
 
-app.use('/add', (req, res, next) => {
-    console.log(req.url);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/add', (req, res, next) => {
     res.send(`<form action='/addbook' method='post'> 
                 <input type='text' name='name' id='name'>
                 <button type='submit'>Add</button>
             </form>`);
 });
 
-app.use('/addbook',(req, res, next) => {
+app.post('/addbook', (req, res, next) => {
     console.log(req.body);
     res.redirect("/");
 });
 
-app.use('/',(req, res, next) => {
-    res.send("Hello!");
+app.use('/', (req, res, next) => {
+    res.send("Hello! You asked for home page or your request didnt match any explicitly mentioned URL's");
 });
 
 app.listen(port, () => {
