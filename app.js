b6925e6c1ae6165e25407629477c735115bc930a
Niranjan <valliveduniranjan@gmail.com>
2020-10-11 23:54:53 +0530

    Implemented ejs template engine along with partial layouts

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require("path");
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-const port = 3000;
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
-app.use('/shop', shopRoutes);
-app.use(express.static(path.join(__dirname,'public')))
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
-app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','not-found.html'))
-})
-
-app.listen(port, () => {
-    console.log("Listening to port: " + port);
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
+
+app.listen(3000);
