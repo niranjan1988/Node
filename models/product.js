c89c02287210b4f5979d319ad6179a2a908dddb0
Niranjan <valliveduniranjan@gmail.com>
2020-10-16 13:02:46 +0530

    Optimising redundant code using callback

--- models/product.js
+++ models/product.js
@@ -1,15 +1,32 @@
-const products=[];
+const fs = require('fs');
+const path = require('path');
+const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
 
-module.exports = class Product {    
+const getProductsFromFile = cb => {
+
+    fs.readFile(p, (err, data) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(data));
+        }
+    });
+}
+
+module.exports = class Product {
     constructor(title) {
         this.title = title;
     }
-
-    save(){
-        products.push(this);
-        console.log(this.products);
+    save() {
+        getProductsFromFile((products) => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
-    static fetchAll(){
-        return products;
+
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
     }
 }
