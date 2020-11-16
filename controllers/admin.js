9a841b5474f40e154071c158744acab0243bd8b0
Niranjan <valliveduniranjan@gmail.com>
2020-11-16 10:31:20 +0530

    Using my sql without sequelize

--- controllers/admin.js
+++ controllers/admin.js
@@ -30,7 +30,7 @@ exports.postEditProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const updatedProduct = new Product(prodId,title,imageUrl,description,price);
+  const updatedProduct = new Product(prodId, title, imageUrl, description, price);
   updatedProduct.save();
   res.redirect('/');
 };
@@ -41,8 +41,8 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  product.save().then(() => res.redirect('/')).catch();
+
 };
 
 exports.getProducts = (req, res, next) => {
--- controllers/shop.js
+++ controllers/shop.js
@@ -2,23 +2,27 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'All Products',
-      path: '/products'
-    });
-  });
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render('shop/product-list', {
+        prods: rows,
+        pageTitle: 'All Products',
+        path: '/products'
+      });
+    })
+    .catch(err => console.log);
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
-    });
-  });
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render('shop/index', {
+        prods: rows,
+        pageTitle: 'Shop',
+        path: '/'
+      });
+    })
+    .catch(err => console.log);
 };
 
 exports.getCart = (req, res, next) => {
@@ -34,25 +38,25 @@ exports.getCart = (req, res, next) => {
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
-        products:cartProducts
+        products: cartProducts
       });
     })
-  }) 
+  })
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId, (product) => {
-    Cart.addProduct(prodId, product.price,()=>{
+    Cart.addProduct(prodId, product.price, () => {
       res.redirect('/cart');
     });
   });
 };
 
-exports.postCartDeleteProduct = (req,res,next)=>{
-  const prodId= req.body.productId;
-  Product.findById(prodId,(product)=>{
-    Cart.deleteProduct(prodId,product.price);
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.deleteProduct(prodId, product.price);
     res.redirect('/cart');
   })
 };
@@ -73,12 +77,15 @@ exports.getCheckout = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    res.render('shop/product-detail', {
-      product: product,
-      pageTitle: product.title,
-      path: '/products'
-    });
-  });
+  Product.findById(prodId)
+  .then(([product]) => {
+      res.render('shop/product-detail', {
+        product: product[0],
+        pageTitle: product.title,
+        path: '/products'
+      });
+    }
+  )
+  .catch();
 };
 
